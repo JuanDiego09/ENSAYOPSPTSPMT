fe0710846aba70eb8402f877c64c50f2d2d09392
victor <vmgarciahurtado@gmail.com>
2018-08-22 09:51:11 -0500

    Victor

--- app/src/main/java/com/example/juan/ensayopsptspmt/actividades/Segunda.java
+++ app/src/main/java/com/example/juan/ensayopsptspmt/actividades/Segunda.java
@@ -1,7 +1,9 @@
 package com.example.juan.ensayopsptspmt.actividades;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.juan.ensayopsptspmt.R;
 
@@ -11,5 +13,31 @@ public class Segunda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(),Tercera.class);
+        Intent intent1 = new Intent(getApplicationContext(),TabedActivity.class);
+        Bundle miBundle = new Bundle();
+
+
+        switch (view.getId()){
+
+            case R.id.btnTimeLog:
+                miBundle.putInt("pantalla",1);
+                intent.putExtra("bundle",miBundle);
+                startActivity(intent);
+                break;
+
+            case R.id.btnDefectLog:
+                miBundle.putInt("pantalla",2);
+                intent.putExtra("bundle",miBundle);
+                startActivity(intent);
+                break;
+
+            case R.id.btnPlanSumary:
+                startActivity(intent1);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/juan/ensayopsptspmt/actividades/Tercera.java
+++ app/src/main/java/com/example/juan/ensayopsptspmt/actividades/Tercera.java
@@ -1,13 +1,34 @@
 package com.example.juan.ensayopsptspmt.actividades;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.juan.ensayopsptspmt.R;
+import com.example.juan.ensayopsptspmt.fragments.DefectLog;
+import com.example.juan.ensayopsptspmt.fragments.TimeLog;
+
 public class Tercera extends AppCompatActivity {
+
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tercera);
+
+        Bundle miBundle = this.getIntent().getBundleExtra("dundle");
+        switch (miBundle.getInt("pantalla")){
+            case 1:
+                fragment = new TimeLog();
+                getSupportFragmentManager().beginTransaction().replace(R.id.contenedorTercera,fragment).commit();
+                break;
+
+            case 2:
+                fragment = new DefectLog();
+                getSupportFragmentManager().beginTransaction().replace(R.id.contenedorTercera,fragment).commit();
+                break;
+        }
+
     }
 }
